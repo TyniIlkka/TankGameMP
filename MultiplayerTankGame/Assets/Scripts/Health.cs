9db7ee71793bcc3fbb1bf65b0c4410e869dd823e
Eetu Maunuksela <eetu.maunuksela@cs.tamk.fi>
2018-11-12 16:45:38 +0200

    *Health display stuff

--- MultiplayerTankGame/Assets/Scripts/Health.cs
+++ MultiplayerTankGame/Assets/Scripts/Health.cs
@@ -7,11 +7,32 @@ namespace TankGame
 {
     public class Health : MonoBehaviour, IDamageReceiver
     {
-        private int _currentHealth;
+        [SerializeField] private float _maxHealth;
+        [SerializeField] private HUD _hud;
 
-        void IDamageReceiver.TakeDamage(int amount)
+        private float _currentHealth;
+
+        void Start()
+        {
+            _currentHealth = _maxHealth;
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                TakeDamage(10);
+            }
+        }
+
+        public void TakeDamage(int amount)
         {
             _currentHealth -= amount;
+
+            if(_hud != null)
+            {
+                _hud.UpdateHealthDisplay(_maxHealth, _currentHealth);
+            }
         }
     }
 }
