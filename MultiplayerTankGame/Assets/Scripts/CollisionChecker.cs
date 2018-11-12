310e7f53d8ed37278331ed1c13832cf400eb53b7
Eetu Maunuksela <eetu.maunuksela@cs.tamk.fi>
2018-11-12 18:04:13 +0200

    *Knockback on Tank collision added

---
+++ MultiplayerTankGame/Assets/Scripts/CollisionChecker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TankGame
+{
+    public class CollisionChecker : MonoBehaviour
+    {
+
+        private TransformMover _mover;
+
+        // Use this for initialization
+        void Start()
+        {
+            _mover = GetComponentInParent<TransformMover>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if(collision.gameObject.layer == LayerMask.NameToLayer("Tank"))
+            {
+                Debug.Log("asd");
+                Vector3 direction = Vector3.Normalize(transform.position - collision.transform.position);
+                direction.y = 0;
+                _mover.StartCoroutine(_mover.Knockback(direction));
+            }
+        }
+    }
+}
--- MultiplayerTankGame/Assets/Scripts/TransformMover.cs
+++ MultiplayerTankGame/Assets/Scripts/TransformMover.cs
@@ -8,6 +8,8 @@ namespace TankGame
     {
         protected float moveSpeed, turnSpeed;
 
+        private bool _knockedBack;
+
         public void Init( float moveSpeed, float turnSpeed )
         {
             this.moveSpeed = moveSpeed;
@@ -16,6 +18,7 @@ namespace TankGame
 
         public void Turn(float amount)
         {
+            if (_knockedBack) return;
             Vector3 rotation = transform.localEulerAngles;
             rotation.y += amount * turnSpeed * Time.deltaTime;
             transform.localEulerAngles = rotation;
@@ -23,6 +26,8 @@ namespace TankGame
 
         public void Move (float amount)
         {
+            if (_knockedBack) return;
+
             Vector3 position = transform.position;
             Vector3 movement = transform.forward * amount * moveSpeed * Time.deltaTime;
             position += movement;
@@ -31,6 +36,8 @@ namespace TankGame
 
         public void Move( Vector3 direction )
         {
+            if (_knockedBack) return;
+
             direction = direction.normalized;
             Vector3 position = transform.position + direction * moveSpeed * Time.deltaTime;
             transform.position = position;
@@ -38,6 +45,8 @@ namespace TankGame
 
         public void Turn( Vector3 target )
         {
+            if (_knockedBack) return;
+
             Vector3 direction = target - transform.position;
             //direction.y = transform.position.y;
             //direction = direction.normalized;
@@ -46,6 +55,26 @@ namespace TankGame
             //transform.rotation = Quaternion.LookRotation(rotation, transform.up);
             Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.up);
             transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+        }
+
+        public IEnumerator Knockback(Vector3 direction)
+        {
+            _knockedBack = true;
+            float knockbackTimer = 1f;
+            float knockBackForce = Random.Range(3f, 5f);
+            float rotDir = Mathf.RoundToInt(Random.value) == 0 ? 100 : -100;
+            while (knockbackTimer > 0)
+            {
+                transform.position += direction * knockBackForce * Time.deltaTime;
+                transform.Rotate(Vector3.up,  rotDir * knockBackForce * Time.deltaTime);
+                knockBackForce -= Time.deltaTime;
+                knockbackTimer -= Time.deltaTime;
+                yield return null;
+            }
+
+            _knockedBack = false;
+            yield return null;
+
         }
     }
 }
