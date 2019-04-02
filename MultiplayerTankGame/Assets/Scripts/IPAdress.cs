9c703fd2bd6cb84bde44962043f10fc819d52a1c
Esa Nord <esa.nord@cs.tamk.fi>
2019-04-02 08:54:50 +0300

    Base project done

---
+++ MultiplayerTankGame/Assets/Scripts/IPAdress.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using System.Net.NetworkInformation;
+
+[RequireComponent(typeof(Text))]
+public class IPAdress : MonoBehaviour {
+
+    private Text textField;
+
+	// Use this for initialization
+	void Start () {
+        textField = GetComponent<Text>();
+        textField.text = IPManager.GetIP(ADDRESSFAM.IPv4);
+	}
+
+}
